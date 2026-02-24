fb1fd35fc4a36ad18178708a5afbee220d44f807
tom <didymusthomas6@gmail.com>
2026-02-24 17:13:35 +0300

    Add todo api skeleton

--- TodoAPI/app.py
+++ TodoAPI/app.py
@@ -4,9 +4,20 @@ import os
 
 app = Flask(__name__)
 
+# Database configs
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_MODIFICATION_TRACK"] = False
 
 db = SQLAlchemy()
+
+# Tables
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    description = db.Column(db.Text)
+    is_completed = db.Column(db.Boolean, default=False)
+
+
+
 
