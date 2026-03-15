191ac44bfebc44e5ca17cd999c293972cdc30cdb
tom <didymusthomas6@gmail.com>
2026-03-15 17:23:53 +0300

    Update calculator with more operations (sub, mul, div)

--- Fundamentals/simple_calculator/app.py
+++ Fundamentals/simple_calculator/app.py
@@ -10,3 +10,26 @@ def add(num1, num2):
         "Result": f"The sum of {num1} and {num2} is {result}."
     }
 
+@app.route("/cal/subtract/<int:num1>/<int:num2>", methods=["POST", "GET"])
+def subtract(num1, num2):
+    result = num1 - num2
+    return {
+        "Result": f"The difference between {num1} and {num2} is {result}."
+    }
+
+@app.route("/cal/multiply/<int:num1>/<int:num2>", methods=["POST", "GET"])
+def multiply(num1, num2):
+    result = num1 * num2
+    return {
+        "Result": f"The product of {num1} and {num2} is {result}."
+    }
+
+@app.route("/cal/divide/<int:num1>/<int:num2>", methods=["POST", "GET"])
+def divide(num1, num2):
+    if num2 == 0:
+        return {"error": f"ZeroDivisionError: Cannot divide {num1} by zero."}
+    result = num1 / num2
+    return {
+        "Result": f"The sum of {num1} and {num2} is {result}."
+    }
+
