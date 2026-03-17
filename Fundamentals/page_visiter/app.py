3ab87a1f27f39ed332745ca7bda5383ae82cc09a
tom <didymusthomas6@gmail.com>
2026-03-17 05:38:56 +0300

    Update page visit counter with clear and reset

--- Fundamentals/page_visiter/app.py
+++ Fundamentals/page_visiter/app.py
@@ -33,6 +33,7 @@ def get_counter():
 def increment_counter():
     if 'counter' in session:
         session['counter'] += 1
+        session.modified = True
         
     return jsonify({
         "counter": session['counter'],
@@ -44,9 +45,28 @@ def increment_counter():
 def decrement_counter():
     if 'counter' in session and session['counter'] > 0:
         session['counter'] -= 1
+        session.modified = True
         
     return jsonify({
         "counter": session['counter'],
         "message": "Counter decremented successfully"
     }, 200)
+    
 
+@app.route("/api/counter/reset", methods=["GET"])
+def reset_counter():
+    session['counter'] = 0
+    session.modified = True
+
+    return jsonify({
+        "session": session['counter'],
+        "message": "session reset successfully"
+    }, 200)
+
+
+@app.route("/api/counter/clear", methods=["GET"])
+def clear_session():
+    session.clear()
+    return jsonify({
+        "message": "Session cleared successfully."
+    }, 200)
